6e547d2cf82f3ee4c133a200eab3988144077a89
Alessandro <alessandro.lekkas@startmail.com>
2020-08-05 14:13:26 +0200

    Add assets and libs

---
+++ app/src/main/java/derp/goforandroid/FAQActivity.java
@@ -0,0 +1,22 @@
+package derp.goforandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+        WebView view = (WebView) findViewById(R.id.contentView);
+        String url = getIntent().getExtras().getString("url");
+        view.loadUrl(url);
+    }
+
+}
